42ddff271f28afb702a57a12db1e86c482012150
bashar4980 <sagor.ahmed3669@gmail.com>
2022-08-31 18:02:15 +0600

    data load is complete

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+const getPhone = async () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  
+  try{
+    const res = await fetch(url);
+  const data = await res.json();
+  displayData(data.data);
+  }
+  catch(error){
+    console.log(error.message)
+  }
+};
 
+const displayData = (items) => {
+  const phoneInfo = document.getElementById("phone_container");
+  items.forEach((item) => {
+    const phoneContainer = document.createElement("div");
+    phoneContainer.classList.add("col-lg-3");
+    phoneContainer.classList.add("col-md-4");
+    phoneContainer.classList.add("col-sm-6");
 
-// 
-
-
-// echo "# phone_hunter" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/bashar4980/phone_hunter.git
-// git push -u origin main
-
-
-// 
+    phoneContainer.innerHTML = `
+        <div class="card">
+            <img src="${item.image}" class="card-img-top p-2" alt="${item.brand}">
+            <div class="card-body">
+                <h5 class="card-title">Brand: ${item.brand}</h5>
+                <h5 class="card-title">Phone Name: ${item.phone_name}</h5>
+               
+                </div>
+        </div>
+        `
+        phoneInfo.appendChild(phoneContainer);
+        console.log(item)
+  });
+};
+getPhone();
