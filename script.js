c0175c75581189e8278767a5bff9e2f303195111
bashar4980 <sagor.ahmed3669@gmail.com>
2022-08-31 17:28:18 +0600

    phone hunter

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// 
+
+
+// echo "# phone_hunter" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/bashar4980/phone_hunter.git
+// git push -u origin main
+
+
+// 
